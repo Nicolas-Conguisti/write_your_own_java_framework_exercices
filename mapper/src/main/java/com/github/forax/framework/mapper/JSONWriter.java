f739181a544fa65b2aa6e20fa010e8bcaf51b79b
Nicolas Conguisti <nicolas.conguisti1@gmail.com>
2026-09-22 09:47:37 +0200

    feat : commmenting the method

--- mapper/src/main/java/com/github/forax/framework/mapper/JSONWriter.java
+++ mapper/src/main/java/com/github/forax/framework/mapper/JSONWriter.java
@@ -1,7 +1,11 @@
 package com.github.forax.framework.mapper;
 
 public final class JSONWriter {
-  public String toJSON(Object o) {
-    throw new UnsupportedOperationException("TODO");
-  }
+
+  /**
+   * Works only with JSON primitive values, null, true, false, any integers or doubles and strings
+   */
+  /*public String toJSON(Object o) {
+    var writer = new JSONWriter();
+  }*/
 }
